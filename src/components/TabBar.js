6cfc59dbe58a48884d36928e99665ea0d93fa81d
PreciseJR <jrmiller525@yahoo.com>
2023-10-17 01:02:37 -0700

    Added image files

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -5,7 +5,7 @@ const TabBar = () => {
   return (
     <div className="border-b border-gray-200 bg-neutral-900 w-screen p-2">
       <div className="flex justify-center">
-        <div className="flex-1 max-w-md ">
+        <div className="flex-1 max-w-md self-center">
           <input
             type="text"
             placeholder="lorens-portfolio"
@@ -13,10 +13,17 @@ const TabBar = () => {
             disabled
           />
         </div>
-        <div className="flex justify-center">
-          <div className="text-white m-2">home</div>
-          <div className="text-white m-2">home</div>
-          <div className="text-white m-2">home</div>
+        <div className="flex">
+          <Link className="bg-search-icon w-6 h-6 bg-no-repeat bg-purple-300"></Link>
+        </div>
+        <div className="flex">
+          <Link className="bg-user-icon w-6 h-6 bg-no-repeat bg-red-300"></Link>
+        </div>
+        <div className="flex">
+          <Link className="bg-search-icon w-6 h-6 bg-no-repeat bg-blue-200"></Link>
+        </div>
+        <div className="flex">
+          <Link className="bg-search-icon w-6 h-6 bg-no-repeat"></Link>
         </div>
       </div>
     </div>
